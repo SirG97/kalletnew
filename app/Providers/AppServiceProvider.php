a9f68d064dc3616aff9f2253212b83ccb333f586
SirG <sirgittarus@gmail.com>
2022-01-01 06:20:23 +0100

    fixed default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Wallet;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Schema::defaultStringLength(191);
         View::composer('*', function($view){
             if(auth()->check()){
                 $balance = Wallet::where('user_id', auth()->user()->id)->first();
